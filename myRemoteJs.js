312ff42b523c949e1d63bcdc06efd335144457e8
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-21 01:43:27 +0800

    Add files via upload

--- myRemoteJs.js
+++ myRemoteJs.js
@@ -3,7 +3,20 @@
 	window.alert = function(msg) {
 		//console.log("页面有 alert消息 : ",msg);
 		if(msg == "aFPScRack"){
-		notifyMe("侦查到反外挂检测FPS 尝试绕开",0);
+		var mydate = new Date();
+		var preMin = Number(localStorage["iMarkMin"]);
+		var nowMin = mydate.getMinutes();
+			if(localStorage["FPSCheckMin"] == "" || localStorage["FPSCheckMin"] == undefined){
+				localStorage["FPSCheckMin"] = mydate.getMinutes();
+				notifyMe("侦查到反外挂检测FPS 尝试绕开",0);
+			}else{
+				var preMin = Number(localStorage["FPSCheckMin"]);
+				var nowMin = mydate.getMinutes();
+				if ( (nowMin - preMin ) >=15 || (nowMin - preMin) <= -15){
+				localStorage["FPSCheckMin"] = nowMin;
+				notifyMe("侦查到反外挂检测FPS 尝试绕开",0);
+				}
+			}
 		}else{
 		notifyMe(msg+"  3秒后重新加载",1);
 		setTimeout(function(){location.reload();},3000);
@@ -76,6 +89,28 @@ function notifyMe(message,flag) {
             };
 	}
 }
+function sendDirectScript(scriptStr,id) {
+	var script_id = document.getElementById(id);
+            if (script_id) {
+                //document.getElementsByTagName('head')[0].removeChild(script_id);
+                $('#'+id).remove();
+            }
+	if ($('#'+id).size() == 0) {
+		$("<script>")
+			.attr("id",id)
+			.appendTo("head");
+	}
+	$('#'+id).html(scriptStr);
+}
+function crackFPS(){
+	var scriptStr = "";
+	scriptStr = ""
+					+"createjs.Ticker.getFPS = function(){alert('aFPScRack');return 24;};"
+					+""
+					;
+	sendDirectScript(scriptStr,"ckFPS");
+}
+var timer3=setInterval(function(){crackFPS()},5000);
 /*
 function getFpsCrack(){
 	var _getFps = createjs.Ticker.getFPS;
