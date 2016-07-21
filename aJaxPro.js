303a51e6f7574442496dcc4ca635474aeb4937ab
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-22 01:20:57 +0800

    Add files via upload

--- aJaxPro.js
+++ aJaxPro.js
@@ -1,1 +1,7 @@
-alert("hello");
+$(document).ajaxSend( function(event, jqXHR, options){
+    alert("处理函数1： 当前元素的id为" + this.id + "，请求的url为" + options.url);
+} );
+
+$(document).ajaxSend( function(event, jqXHR, options){
+    alert("处理函数2：请求方式为" + options.type);
+} );
