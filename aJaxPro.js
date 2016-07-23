c7f5d8164fd5e49bd499aef979b9a02b1becdd80
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-24 04:20:11 +0800

    Add files via upload

--- aJaxPro.js
+++ aJaxPro.js
@@ -7,7 +7,7 @@ function chAjaxK(){
 		$.ajaxSettings.beforeSend = function(a,b){
 			console.info(b.url);
 			var target = "ob?t";
-			if(b.url.match(target)){
+			if(target.match(b.url)){
 				console.info('检测到 ob?t=,尝试拦截',b.url);
 				xhr.abort();
 				return false;
