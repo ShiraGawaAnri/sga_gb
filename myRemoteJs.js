dd30ef5855993d3c37069ddfbed6767a583b826c
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-25 02:57:09 +0800

    Add files via upload

--- myRemoteJs.js
+++ myRemoteJs.js
@@ -9,8 +9,8 @@
 		var nowMin = mydate.getMinutes();
 			if(localStorage["pRotectCheckMin"] == "" || localStorage["pRotectCheckMin"] == undefined){
 				localStorage["pRotectCheckMin"] = mydate.getMinutes();
-					notifyMe("保护机制--ver0.7.24--运行中",0);
-					setTimeout(function(){notifyMe("已加载防护：\n<--FPS重载 Tap解除 $.ajax拦截-->",1)},5000);
+					notifyMe("保护机制--ver0.7.25--运行中",0);
+					setTimeout(function(){notifyMe("已加载防护:\n<--FPS重载 Tap解除 $.ajax拦截-->",1)},5000);
 					
 			}else{
 				var preMin = Number(localStorage["pRotectCheckMin"]);
@@ -18,7 +18,7 @@
 				if ( (nowMin - preMin ) >=15 || (nowMin - preMin) <= -15){
 				localStorage["pRotectCheckMin"] = nowMin;
 					notifyMe("保护机制--ver0.7.24--运行中",0);
-					setTimeout(function(){notifyMe("已加载防护：\n<--FPS重载 Tap解除 $.ajax拦截-->",1)},5000);
+					setTimeout(function(){notifyMe("已加载防护:\n<--FPS重载 Tap解除 $.ajax拦截-->",1)},5000);
 				}
 			}
 		}else if(msg == "aJaxhAsCk"){
