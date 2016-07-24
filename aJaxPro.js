ee481bf2518ecc4c6792792f8cbdd084435cc119
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-24 16:08:37 +0800

    Add files via upload

--- aJaxPro.js
+++ aJaxPro.js
@@ -5,10 +5,10 @@ function chAjaxK(){
 	if($.ajaxSettings.beforeSend != undefined){
 		hookAjaxSettingsBeforeSend = $.ajaxSettings.beforeSend;
 		$.ajaxSettings.beforeSend = function(a,b){
-			console.info(b.url);
+			//console.info(b.url);
 			//show
-			if(b.url.match(/ob\?t/)){
-				console.info('检测到 ob?t=,尝试拦截url =',b.url,'的$.ajax请求');
+			if(b.url.match(/ob\?t/)|| b.url.match("ob")){
+				console.info('检测到 ob,尝试拦截url =',b.url,'的$.ajax请求');
 				if($('script[id^="IFmyAlert"]').length > 0){
 					alert("aJaxhAsCk");
 				}
