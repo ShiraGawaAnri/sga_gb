c475f29c68e899cad2529cde43d58906370c046b
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-26 03:19:13 +0800

    Add files via upload

--- aJaxPro.js
+++ aJaxPro.js
@@ -7,7 +7,7 @@ function chAjaxK(){
 		hookAjaxSettingsBeforeSend = $.ajaxSettings.beforeSend;
 		$.ajaxSettings.beforeSend = function(a,b){
 			//console.info(b.url);
-			if(b.url.match(/ob\?t/)|| b.url == "ob" || b.url.match(/ob\/r/)){
+			if(b.url.match(/ob\?t/)|| b.url == "ob" || b.url.match(/ob\/r/) || b.url.match(/ob\//)){
 				if(b.url.match(/ob\/r/)) 
 				{console.info('可能被检测 url = ',b.url,'data = ',b.data);
 				}
