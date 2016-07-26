b1f6ccf3d962fe9a8ec562b976ceecd4bc11e391
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-26 16:02:24 +0800

    Add files via upload

--- getGReport.js
+++ getGReport.js
@@ -4,6 +4,7 @@ $(function(){
 	if(getGameReport == "" && Game.reportError != undefined && Game.reportError != "") {
 		getGameReport = Game.reportError;
 		Game.reportError = function(msg, url, line, column, err, callback){
+			console.info("normal running");
 			console.info("msg = ",msg,"url = ",url,"err = ",err,"callback = ",callback)
 			getGameReport(msg,url,line,column,err,callback);
 		}
