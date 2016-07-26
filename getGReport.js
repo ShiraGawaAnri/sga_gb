38484638cab9648dd44001cc31964ac03beff8ea
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-26 15:43:03 +0800

    Add files via upload

---
+++ getGReport.js
@@ -0,0 +1,11 @@
+﻿var getGameReport = "";
+console.info("start")
+$(function(){
+	if(getGameReport == "" && Game.reportError != undefined && Game.reportError != "") {
+		getGameReport = Game.reportError;
+	}
+	Game.reportError = function(msg, url, line, column, err, callback){
+		console.info("msg = ",msg,"url = ",url,"err = ",err,"callback = ",callback)
+		getGameReport(msg,url,line,column,err,callback);
+	}
+})
