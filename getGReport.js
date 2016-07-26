bfab34c0732e342968398e2c4075cc524c36d3aa
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-26 15:56:40 +0800

    Add files via upload

--- getGReport.js
+++ getGReport.js
@@ -1,33 +1,4 @@
-﻿function sendDirectScriptG(scriptStr,id) {
-	var script_id = document.getElementById(id);
-            if (script_id) {
-                $('#'+id).remove();
-            }
-	if ($('#'+id).size() == 0) {
-		$("<script>")
-			.attr("id",id)
-			.appendTo("body");
-	}
-	$('#'+id).html(scriptStr);
-}
-
-var tmpStr = ''
-			+'var getGameReport = "";'
-			+'console.info("start")'
-			+'$(function(){'
-			+'if(getGameReport == "" && Game.reportError != undefined && Game.reportError != "") {'
-			+'getGameReport = Game.reportError;'
-			+'Game.reportError = function(msg, url, line, column, err, callback){'
-			+'console.info("msg = ",msg,"url = ",url,"err = ",err,"callback = ",callback)'
-			+'getGameReport(msg,url,line,column,err,callback);'
-			+'}'
-			+'}'
-			+'})'
-			+''
-			;
-sendDirectScriptG(tmpStr,"gGRTest");
-/*
-var getGameReport = "";
+﻿var getGameReport = "";
 console.info("start")
 $(function(){
 	if(getGameReport == "" && Game.reportError != undefined && Game.reportError != "") {
@@ -38,4 +9,3 @@ $(function(){
 		}
 	}
 })
-*/
