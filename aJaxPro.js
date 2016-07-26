2be676c4b65b6c7d0f04a6b7fdce00a19f539c18
ShiraGawaAnri <soranorio911128@gmail.com>
2016-07-26 12:52:05 +0800

    Add files via upload

--- aJaxPro.js
+++ aJaxPro.js
@@ -10,22 +10,22 @@
 						console.info('可能被检测 url = ', b.url, 'data = ', b.data);
 						//console.info('test = ',JSON.stringify(b.data).split(",")[0]);
 						var codeT = "";
-						if (JSON.stringify(b.data).split(",")[0].match(/"1001/)) {
+						if (JSON.stringify(b.data).split(",")[0].match(/1001/)) {
 							codeT = "1001";
 							console.info("模拟点击ob被拦截 危险:低");
-						} else if (JSON.stringify(b.data).split(",")[0].match(/"7001/)) {
+						} else if (JSON.stringify(b.data).split(",")[0].match(/7001/)) {
 							codeT = "7001";
 							console.info("加速ob被拦截 危险:高");
-						} else if (JSON.stringify(b.data).split(",")[0].match(/"8001/)) {
+						} else if (JSON.stringify(b.data).split(",")[0].match(/8001/)) {
 							codeT = "8001";
 							console.info("ViramateUser(维拉)ob被拦截 危险:不确定能完全拦截");
-						} else if (JSON.stringify(b.data).split(",")[0].match(/"9001/)) {
+						} else if (JSON.stringify(b.data).split(",")[0].match(/9001/)) {
 							codeT = "9001";
 							console.info("gbftool(日版小哔)ob被拦截 危险:低");
-						} else if (JSON.stringify(b.data).split(",")[0].match(/"9002/)) {
+						} else if (JSON.stringify(b.data).split(",")[0].match(/9002/)) {
 							codeT = "9002";
 							console.info("gfe(国内某辅助）ob被拦截 危险:不确定能完全拦截");
-						} else if (JSON.stringify(b.data).split(",")[0].match(/"9003/)) {
+						} else if (JSON.stringify(b.data).split(",")[0].match(/9003/)) {
 							codeT = "9003";
 							console.info("guraburu(不明) ob被拦截 危险:不确定能完全拦截");
 						} else {
